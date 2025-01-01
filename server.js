e806040657df92d865aab87dbfe57d8b28881ccd
Michael Bondarevsky <bondarevsky@gmail.com>
2025-01-01 13:27:30 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    console.log('new connection');
+    res.end("Hello form server")
+})
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, ()=> console.log('listening'));
+
